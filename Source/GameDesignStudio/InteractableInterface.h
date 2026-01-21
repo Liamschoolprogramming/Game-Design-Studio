67e6e7d7c2e744fb104a849ae805291ac45d0c92
Liam Fishman <johnliamfishmanschool@gmail.com>
2026-01-21 02:05:41 -0800

    small interface change

--- Source/GameDesignStudio/InteractableInterface.h
+++ Source/GameDesignStudio/InteractableInterface.h
@@ -3,15 +3,27 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/Object.h"
+#include "UObject/Interface.h"
 #include "InteractableInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI, Blueprintable)
+class UInteractableInterface : public UInterface
+{
+	GENERATED_BODY()
+};
 
 /**
  * 
  */
-UCLASS()
-class GAMEDESIGNSTUDIO_API UInteractableInterface : public UObject
+class GAMEDESIGNSTUDIO_API IInteractableInterface
 {
 	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void Interact(UObject* SourceObj);
 	
 };
