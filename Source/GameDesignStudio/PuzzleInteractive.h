a1fbcb3596c4d9df18d026dee86ac8eda92977ae
game029 <game029@mclab.local>
2026-01-21 18:26:07 -0800

    added puzzle baseclasses

---
+++ Source/GameDesignStudio/PuzzleInteractive.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Puzzle.h"
+#include "PuzzleInteractive.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEDESIGNSTUDIO_API APuzzleInteractive : public APuzzle
+{
+	GENERATED_BODY()
+	
+};
