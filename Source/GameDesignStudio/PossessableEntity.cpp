94e3a03200c3764a76efc63b829df78cdfdf7393
Alirie Gray <alirie.gray@gmail.com>
2026-01-26 13:31:08 -0800

    Add camera control to possessable

--- Source/GameDesignStudio/PossessableEntity.cpp
+++ Source/GameDesignStudio/PossessableEntity.cpp
@@ -2,7 +2,9 @@
 
 
 #include "PossessableEntity.h"
-
+#include "PlayerCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 #include "Kismet/GameplayStatics.h"
 
 // Sets default values
@@ -10,13 +12,30 @@ APossessableEntity::APossessableEntity()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+	//Initialize camera arm
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->TargetArmLength = 800;
 
+	//Setup camera arm attach to the root component of the class
+	CameraBoom->SetupAttachment(RootComponent);
+
+	//use the pawn control rotation
+	CameraBoom->bUsePawnControlRotation = true;
+
+	//Initialize Camera
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+
+	//Set attach to the camera boom
+	FollowCamera->SetupAttachment(CameraBoom);
 }
 
 // Called when the game starts or when spawned
 void APossessableEntity::BeginPlay()
 {
 	Super::BeginPlay();
+	PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	PlayerCharacter = Cast<APlayerCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(),0));
 }
 
 // Called every frame
@@ -25,15 +44,57 @@ void APossessableEntity::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 }
 
+void APossessableEntity::OnPossess()
+{
+	PlayerController->Possess(this);
+}
+
+void APossessableEntity::OnCancelPossess()
+{
+	PlayerController->Possess(PlayerCharacter);
+}
+
+void APossessableEntity::MoveForward(float AxisValue)
+{
+	if ((Controller != NULL) && (AxisValue != 0.0f))
+	{
+		//find which direction is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		//get forward vector
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		//add movement in that direction
+		AddMovementInput(Direction, AxisValue);
+	}
+}
+
+void APossessableEntity::MoveRight(float AxisValue)
+{
+	if ((Controller != NULL) && (AxisValue != 0.0f))
+	{
+		//find which direction is right
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		//get right vector
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		//add movement in that direction
+		AddMovementInput(Direction, AxisValue);
+	}
+}
+
 // Called to bind functionality to input
 void APossessableEntity::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-}
+	
+	//bind move
+	PlayerInputComponent->BindAxis("MoveForward", this, &APossessableEntity::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APossessableEntity::MoveRight);
 
-void APossessableEntity::OnPossess()
-{
-	APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
-	PlayerController->Possess(this);
+	//bind look
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 }
-
--- Source/GameDesignStudio/PossessableEntity.h
+++ Source/GameDesignStudio/PossessableEntity.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "PlayerCharacter.h"
 #include "GameFramework/Character.h"
 #include "PossessableEntity.generated.h"
 
@@ -20,12 +21,33 @@ public:
 	
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	
+	void MoveForward(float AxisValue);
+
+	void MoveRight(float AxisValue);
+	
+	UPROPERTY()
+	APlayerController* PlayerController;
+	
+	UPROPERTY()
+	APlayerCharacter* PlayerCharacter;
+	
 	UFUNCTION(BlueprintCallable)
 		void OnPossess();
+	
+	UFUNCTION(BlueprintCallable)
+		void OnCancelPossess();
 	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 	
+	//Camera arm
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Components")
+	USpringArmComponent* CameraBoom;
+
+	//Camera
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Components")
+	UCameraComponent* FollowCamera;
+
 
 };
