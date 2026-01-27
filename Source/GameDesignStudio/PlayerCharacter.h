e8a48fcee3f2559f0c0c4eeee599e40483af9a96
JoelioBrolio <mrgalliminus@gmail.com>
2026-01-26 17:31:58 -0800

    Spiked Wall Implementation
    very basic. knockback is based on player position - spiked wall position and multiplied by a power value. credit to Box for helping me find out where the vector was being set to 0 😭

--- Source/GameDesignStudio/PlayerCharacter.h
+++ Source/GameDesignStudio/PlayerCharacter.h
@@ -38,6 +38,11 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	// i want this to be called in blueprints as well so im putting it here :)
+	// this is so knockback can be done by anything and the code doesnt need to be rewritten like a thousand times
+	UFUNCTION(BlueprintCallable)
+	void DoKnockback(float Power, AActor* origin);
+
 };
 
 
