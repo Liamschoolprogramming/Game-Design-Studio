894e973475fbc14f557b1e10082a28fdc9e05ed2
Liam Fishman <johnliamfishmanschool@gmail.com>
2026-01-28 03:40:47 -0800

    defining some methods

--- Source/GameDesignStudio/Core/Subsystems/StartingWorldSubsystem.h
+++ Source/GameDesignStudio/Core/Subsystems/StartingWorldSubsystem.h
@@ -4,7 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "Subsystems/WorldSubsystem.h"
+#include "core/Puzzles/Puzzle.h"
 #include "StartingWorldSubsystem.generated.h"
+
+// Maybe rename to something broad since I think this will be spawned each level
+// then have the level managers be unique
+
+class APuzzle;
 
 /**
  * 
@@ -13,4 +19,17 @@ UCLASS()
 class GAMEDESIGNSTUDIO_API UStartingWorldSubsystem : public UWorldSubsystem
 {
 	GENERATED_BODY()
+	
+	
+public:
+	virtual void initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+	
+	void RegisterPuzzleActor(APuzzle* Actor);
+	void UnregisterPuzzleActor(APuzzle* Actor);
+	
+	void ApplyState(APuzzle* Actor);
+	
+private:
+	TMap<FPuzzleId, TWeakObjectPtr<APuzzle>> ActiveActors;
 };
