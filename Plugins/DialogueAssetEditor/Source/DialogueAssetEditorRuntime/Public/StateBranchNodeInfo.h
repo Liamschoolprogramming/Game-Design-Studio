e0caf2745081efd6b533bda7f9d09147b26ec307
systeminabox <systeminabox28@gmail.com>
2026-03-03 17:00:46 -0800

    state switch wip

---
+++ Plugins/DialogueAssetEditor/Source/DialogueAssetEditorRuntime/Public/StateBranchNodeInfo.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DialogueNodeInfoBase.h"
+#include "StateBranchNodeInfo.generated.h"
+
+
+
+/**
+ * 
+ */
+UCLASS()
+class DIALOGUEASSETEDITORRUNTIME_API UStateBranchNodeInfo : public UDialogueNodeInfoBase
+{
+	GENERATED_BODY()
+
+public:
+	//Properties go here
+	UPROPERTY(EditAnywhere, DisplayName= "States")
+	TArray<FText> EditableStatesResponses;
+	
+	UPROPERTY()
+	FText StartState = FText::FromString(TEXT("FirstTime"));
+	
+	
+};
