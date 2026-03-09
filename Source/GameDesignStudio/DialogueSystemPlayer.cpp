fb5b9f4141ecf33e9d6c30a15818e807ee09d1dd
systeminabox <systeminabox28@gmail.com>
2026-03-09 13:32:19 -0700

    multi path nodes

--- Source/GameDesignStudio/DialogueSystemPlayer.cpp
+++ Source/GameDesignStudio/DialogueSystemPlayer.cpp
@@ -80,7 +80,7 @@ UDialogueSpeakerComponent* UDialogueSystemPlayer::FindSpeakerComponent(UWorld* W
 		if (It->GetWorld() != World)
 			continue;
 
-		if (It->ComponentHasTag(SpeakerName))
+		if (It->SpeakerName == SpeakerName)
 		{
 			return *It;
 		}
@@ -264,8 +264,7 @@ void UDialogueSystemPlayer::ChangeCamera(FName CameraName, float TransitionTime,
 		
 	}else
 	{
-		
-		ChooseFirstOption();
+		ChooseOptionAtIndex(0);
 	}
 }
 
