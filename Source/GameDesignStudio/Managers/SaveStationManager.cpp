cfae08d0821a3768cb39d1d949b6211ddf298c29
petangvil <pinkpinkpinkpynk@gmail.com>
2026-03-10 13:42:53 -0700

    Tutorial Killzone & UI Fixes + SaveStation Debug Tool (#386)
    * Tutorial Kill Zone Fix
    
    - Created new BP_PuzzleKillZone. The other one seemed  to have been there but wasn't functioning so I made a quick blueprint version to serve as a temporary fix.
    
    * Mouse Cursor UI Bug
    
    - Fixes issue where mouse cursor is hidden on Chest Reward Display and Respawn Screen
    
    * Save Station Debug Cycling
    
    - You can cycle between save stations with + and -. If the  level has no save stations, doing so will respawn you at the start.
    
    * Reapply Tutorial Killzone Fix & Update Collisions
    
    - Reapply fix for tutorial killzone
    - Update collision on tutorial walls

--- Source/GameDesignStudio/Managers/SaveStationManager.cpp
+++ Source/GameDesignStudio/Managers/SaveStationManager.cpp
@@ -7,6 +7,7 @@
 #include "Macros.h"
 #include "Core/Subsystems/GameManagerSubsystem.h"
 #include "Blueprint/UserWidget.h"
+#include "EngineUtils.h"
 #include "Engine/RendererSettings.h"
 #include "Kismet/GameplayStatics.h"
 
@@ -15,5 +16,73 @@ void USaveStationManager::Initialize(UGameManagerSubsystem* InstanceOwner)
 	Super::Initialize(InstanceOwner);
 	
 	ActiveSaveStation = nullptr;
+	SaveStations = GetSaveStations();
+	CurrentSaveStationIndex = 0;
 }
 
+/**
+ * Sets the save station that the player respawns at.
+ * @param SaveStation 
+ */
+void USaveStationManager::SetActiveSaveStation(ASaveStation* SaveStation)
+{
+	SaveStations = GetSaveStations();
+	
+	CurrentSaveStationIndex = SaveStations.Find(SaveStation);
+	ActiveSaveStation = SaveStation;
+}
+
+/**
+ * @return an array of every save station in the world.
+ */
+TArray<ASaveStation*> USaveStationManager::GetSaveStations()
+{
+	TArray<ASaveStation*> Stations;
+
+	for (TActorIterator<ASaveStation> Iterator(GetWorld()); Iterator; ++Iterator)
+	{
+		Stations.Add(*Iterator);
+	}
+
+	return Stations;
+}
+
+/* *
+ * Updates the players save station to be the next one in list.
+*/
+void USaveStationManager::NextSaveStation()
+{
+	SaveStations = GetSaveStations();
+	if (SaveStations.Num() < 1)
+	{
+		return;
+	}
+	
+	int NextIndex = CurrentSaveStationIndex+1;
+	if (NextIndex >= SaveStations.Num())
+	{
+		NextIndex = 0;
+	}
+	
+	SetActiveSaveStation(SaveStations[NextIndex]);
+}
+
+/* *
+ * Updates the players save station to be the previous one in list.
+*/
+void USaveStationManager::PreviousSaveStation()
+{
+	SaveStations = GetSaveStations();
+	if (SaveStations.Num() < 1)
+	{
+		return;
+	}
+	
+	int PreviousIndex = CurrentSaveStationIndex-1;
+	if (PreviousIndex < 0)
+	{
+		PreviousIndex = SaveStations.Num()-1;
+	}
+	
+	SetActiveSaveStation(SaveStations[PreviousIndex]);
+}
